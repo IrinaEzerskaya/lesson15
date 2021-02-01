918318b8fdb15cdc8227872cae33af5db5a2c280
Irina Ezerskaya <titira217@mail.ru>
2021-02-01 23:26:12 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Basket myShopBasket = new Basket();
+
+        myShopBasket.addProduct("item 1", 100);
+        myShopBasket.addProduct("item 2", 200);
+        myShopBasket.addProduct("item 3", 300);
+
+        List<String> items = myShopBasket.getProducts();
+        System.out.println("Товары в корзине");
+        System.out.println(items);
+
+        System.out.println("Кол-во товара item 2 до обновления");
+        System.out.println(myShopBasket.getProductQuantity("item 2"));
+
+        myShopBasket.updateProductQuantity("item 2", 250);
+
+        System.out.println("Кол-во товара item 2 после обновления");
+        System.out.println(myShopBasket.getProductQuantity("item 2"));
+
+        System.out.println("Кол-во товаров после очистки корзины");
+        myShopBasket.clear();
+        System.out.println(myShopBasket.getProducts());
+    }
+}
